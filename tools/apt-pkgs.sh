8156b2486bbf4714b5dd6394cb02d13873078c88
Curtis Bowman <c.bowman1711@gmail.com>
2023-03-26 03:08:53 -0400

    update apt-packages script and list

--- tools/apt-pkgs.sh
+++ tools/apt-pkgs.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-
-
-
-
+# list all apt packages installed manually
 comm -23 <(apt-mark showmanual | sort -u) \
-         <(gzip -dc /var/log/installer/initial-status.gz |
-           sed -n 's/^Package: //p' | sort -u)
+    <(gzip -dc /var/log/installer/initial-status.gz |
+        sed -n 's/^Package: //p' |
+        sort -u)
